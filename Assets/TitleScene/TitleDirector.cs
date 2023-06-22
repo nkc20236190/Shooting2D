9a5d6433850331522af43d626d3b82a8ed479231
nkc20236190 <nkc23.uno.yudai@gmail.com>
2023-06-22 11:11:44 +0900

    6月21日更新

---
+++ Assets/TitleScene/TitleDirector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleDirector : MonoBehaviour
+{
+    public Text scoreLabel; // 前回のスコア（距離）表示
+
+    void Start()
+    {
+        scoreLabel.text = "Score\n" + GameDirector.kyori.ToString("D6");
+    }
+
+    void Update()
+    {
+        // 左クリックまたはゲームパッドのボタン０でスタート
+        if(Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
